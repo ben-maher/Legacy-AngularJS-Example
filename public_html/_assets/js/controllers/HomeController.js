a42fbabe11a69514ac4c15e38fd36738c4e9732c
Ben Maher <benmaher@protonmail.com>
2016-02-25 16:31:11 +0000

    Re-wrote home to get real content

--- public_html/_assets/js/controllers/HomeController.js
+++ public_html/_assets/js/controllers/HomeController.js
@@ -1,9 +1,43 @@
-angular.module('ImdbRip').controller('HomeController', function ($scope, $routeParams) {
-                $scope.name = "Home Controller";
-                $scope.params = $routeParams;
+angular.module('ImdbRip').controller('HomeController', function ($scope, $routeParams, $http) {
+    $scope.name = "Home Controller";
+    $scope.params = $routeParams;
 
-                //todo create array of movies to render on the homepage (api to fetch details only supplying their imdbid.
-                $scope.newFilms = [];
-                $scope.popularFilms = [];
+    //todo create array of movies to render on the homepage (api to fetch details only supplying their imdbid.
+    $scope.newFilms = [];
+    $scope.popularFilms = [];
 
-            });
+    ["tt2404233", "tt1431045"].forEach(function (value) {
+
+        var imdbReq = {
+            method: 'GET',
+            url: 'https://omdbapi.com/?i=' + value + '&v=1'
+        };
+
+        $http(imdbReq).success(function (data) {
+            $scope.newFilms.push(data);
+
+        });
+
+    });
+
+    ["tt0120655", "tt0077415", "tt0087800", "tt0317740", "tt0099685", "tt2278871"].forEach(function (value) {
+
+        var imdbReq = {
+            method: 'GET',
+            url: 'https://omdbapi.com/?i=' + value + '&v=1'
+        };
+
+        $http(imdbReq).success(function (data) {
+            $scope.popularFilms.push(data);
+
+        });
+
+    });
+    
+    
+    $scope.Navigate = function(imdbItem) {
+        window.location = "#/details/" + imdbItem.imdbID;
+};
+
+
+});
