296a3d97003fef4187026ce90c79599e57216210
Ben Maher <bmaher@protonmail.com>
2016-02-27 15:08:11 +0000

    Implementation of cast inside movie controller.

--- public_html/_assets/js/controllers/MovieController.js
+++ public_html/_assets/js/controllers/MovieController.js
@@ -1,12 +1,24 @@
-angular.module('ImdbRip').controller('MovieController', function ($scope, $routeParams, $http) {                
-                var movieById = {
-                    type: 'GET',
-                    url: 'http://api.themoviedb.org/3/movie/'+$routeParams.id+'?api_key=4b9adfc40dafb4edd660a77a53f04129'
-                };
-                
-                $http(movieById).success(function(data){
-                   $scope.movie = data; 
-                });
-                
-            });
+angular.module('ImdbRip').controller('MovieController', function ($scope, $routeParams, $http) {
+    var movieById = {
+        type: 'GET',
+        url: 'http://api.themoviedb.org/3/movie/' + $routeParams.id + '?api_key=4b9adfc40dafb4edd660a77a53f04129'
+    };
+    var creditsByMovie = {
+        type: 'GET',
+        url: 'http://api.themoviedb.org/3/movie/' + $routeParams.id + '/credits?api_key=4b9adfc40dafb4edd660a77a53f04129'
+    };
+
+    $http(movieById).success(function (data) {
+        $scope.movie = data;
+        
+            $http(creditsByMovie).success(function (data) {
+        $scope.movie.credits = data;
+        console.log($scope.movie);
+    });
+        
+    });
+    
+
+    
+});
             
