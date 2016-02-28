0b60bfae258d60a15b9c3d355b80d29b70660a9a
Ben Maher <bmaher@protonmail.com>
2016-02-28 11:15:50 +0000

    Added movie reviews

--- public_html/_assets/js/controllers/MovieController.js
+++ public_html/_assets/js/controllers/MovieController.js
@@ -7,18 +7,29 @@ angular.module('ImdbRip').controller('MovieController', function ($scope, $route
         type: 'GET',
         url: 'http://api.themoviedb.org/3/movie/' + $routeParams.id + '/credits?api_key=4b9adfc40dafb4edd660a77a53f04129'
     };
+    
+    var reviewsByMovie = {
+        type: 'GET',
+        url: 'http://api.themoviedb.org/3/movie/' + $routeParams.id + '/reviews?api_key=4b9adfc40dafb4edd660a77a53f04129'
+    };
 
     $http(movieById).success(function (data) {
         $scope.movie = data;
-        
-            $http(creditsByMovie).success(function (data) {
-        $scope.movie.credits = data;
-        console.log($scope.movie);
-    });
-        
-    });
-    
 
-    
+        $http(creditsByMovie).success(function (data) {
+            $scope.movie.credits = data;
+            
+           $scope.movie.genresString = $scope.movie.genres.map(function(genre) { return genre['name']; });
+        });
+        
+        $http(reviewsByMovie).success(function (data) {
+            $scope.movie.reviews = data;
+            console.log($scope.movie);
+        });
+
+    });
+
+
+
 });
             
