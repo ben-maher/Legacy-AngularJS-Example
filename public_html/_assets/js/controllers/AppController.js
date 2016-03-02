4cd93a305de733bfcf3bb1fcc3da5b851f426977
Ben Maher <benmaher@protonmail.com>
2016-03-02 12:29:09 +0000

    Added support to onRoute for startsWith checks

--- public_html/_assets/js/controllers/AppController.js
+++ public_html/_assets/js/controllers/AppController.js
@@ -2,11 +2,13 @@ angular.module('ImdbRip').controller('AppController', function ($route, $locatio
     this.onRoute = function (routeName) {
         var routeChecks = routeName instanceof Array ? routeName : arguments;
         var currentRouteName = $route.current && $route.current.name ? $route.current.name : null;
+        var isPrefix = routeName.substr(-1)==='*';
+        if(isPrefix) routeName = routeName.replace('*','');
         if (currentRouteName)
         {
             for (var i = 0, c = routeChecks.length; i < c; i++)
             {
-                if (routeChecks[i] === currentRouteName)
+                if (routeChecks[i] === currentRouteName || (isPrefix && currentRouteName.startsWith(routeChecks[i])))
                     return true;
             }
             return false;
